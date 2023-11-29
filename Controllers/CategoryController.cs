dce96882cf8ef5c5d38506eb901fe2f7ba11e26b
Vinicius <vini383@gmail.com>
2023-11-29 17:17:56 -0300

    feat: added GetCategoryById endpoint

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,13 +1,34 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        [HttpGet("categories")]
-        public IActionResult Get([FromServices] BlogDataContext context) => Ok(context.Categories.ToList());
+        [HttpGet("v1/categories")]
+        public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
+        {
+            List<Category> categories = await context.Categories.ToListAsync();
+            return Ok(categories);
+
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync([FromRoute] int id, [FromServices] BlogDataContext context)
+        {
+            Category selectedCategory = await context.Categories.FirstOrDefaultAsync(cat => cat.Id == id);
+            if (selectedCategory != null)
+            {
+                return Ok(selectedCategory);
+            }
+
+            return NotFound();
+
+
+        }
 
     }
 }
