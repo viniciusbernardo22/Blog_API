7426684907c0dc18d25f3488f9eba70e66792362
Vinicius <vini383@gmail.com>
2023-11-30 15:23:26 -0300

    feat: added ResultViewModel

--- ViewModels/EditorCategoryViewModel.cs
+++ ViewModels/EditorCategoryViewModel.cs
@@ -7,6 +7,7 @@ namespace Blog.ViewModels
         [Required(ErrorMessage = "O nome é obrigatório")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "O slug é obrigatório")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
         public string Slug { get; set; }
