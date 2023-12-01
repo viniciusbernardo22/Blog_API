2a316980c4a1b08fdf08f26853b17a45c85bf64d
Vinicius <vini383@gmail.com>
2023-12-01 11:55:45 -0300

    feat: added AccountController and login endpoint implementation

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly TokenService _tokenService;
+    
+    public AccountController(TokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+    
+    [HttpPost("v1/login")]
+    public IActionResult Login()
+    {
+        var token = _tokenService.GenerateToken(null);
+        return Ok(token);
+    }
+}
